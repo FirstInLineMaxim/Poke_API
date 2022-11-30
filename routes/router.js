9dc0e660afb65e59d83050bcd9613e0e5b5f2480
Frank Asamoah <frank.asamoah119@gmail.com>
2022-11-30 17:54:18 +0100

    info

--- routes/router.js
+++ routes/router.js
@@ -15,7 +15,11 @@ router.get('/',(req, res) => {
   res.send(pokemonData)
 }).get('/:id',(req, res) => {
   res.send(getPokemonById(parseInt(req.params.id), pokemonData))
-})
+}).get('/:id/:info', (req, res) => {
+  const {id, info} = req.params;
+  const getInfo = getPokemonById(parseInt(id), pokemonData)[info];
+  res.send(getInfo);
+  })
 
 
 
