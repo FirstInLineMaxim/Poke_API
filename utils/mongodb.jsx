294c46dd6afff9ac87d0987a9a1ef769f2333a73
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-11-30 20:14:00 +0100

    mongodb

---
+++ utils/mongodb.jsx
@@ -0,0 +1,8 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://StreatRazor:<password>@pokefight.y38z2e2.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
