67c4a14c3857502551eb9fac7779c2dc181b83f5
frankasamoah <96752421+frankasamoah@users.noreply.github.com>
2022-12-01 18:33:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 4444;
 app.use(cors())
 app.use("/api/v1/pokemon", router)
 
+// if no routes are matched, show this || "*" is a wildcard
+app.use('*', (req, res) => res.status(404).json({error: "not found"}))
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
